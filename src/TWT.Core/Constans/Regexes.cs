1b23895f21c386fe3bc0459638932864973ee4ab
Bali Zsolt <gilamail55@gmail.com>
2023-01-15 20:35:43 +0100

    Fix Model problems

--- src/TWT.Core/Constans/Regexes.cs
+++ src/TWT.Core/Constans/Regexes.cs
@@ -2,7 +2,7 @@
 {
     public static class Regexes
     {
-        public static readonly string LincensePlateRegex = "^.{3}-[0-9]{3}$";
+        public static readonly string LincensePlateRegex = "^[A-Z a-z]{3}-[0-9]{3}$";
         public static readonly string OwnerNameRegex = "^[a-z A-Z]{3,20} [a-z A-Z]{3,20}$";
     }
 }
--- src/TWT.Data/Models/Car.cs
+++ src/TWT.Data/Models/Car.cs
@@ -9,13 +9,14 @@ namespace TWT.Data.Models
     {
         [Key]
         [Column("LincensePlate")]
-        [RegularExpression("^.{3}-[0-9]{3}$")]//abc-123
+        [RegularExpression("^[A-Z a-z]{3}-[0-9]{3}$")]//abc-123
         public string LincensePlate { get; set; }
 
         [Column("OwnerName")]
         [RegularExpression("^[a-z A-Z]{3,20} [a-z A-Z]{3,20}$")]
         public string OwnerName { get; set; }
 
+        [Range(1,double.MaxValue)]
         [Column("Power")]
         public int Power { get; set; }
     }
